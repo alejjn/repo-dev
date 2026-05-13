1df113c61856cdc0d145e0b9c40328f8f77ef9ce
distrito-gta <distritogta@gmail.com>
2026-05-13 14:12:29 -0300

    Fix static assets copy in Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
     react({
       include: '**/*.{jsx,js}',
     }),
+    viteStaticCopy({
+      targets: [
+        { src: 'assets/images', dest: 'assets' },
+        { src: 'assets/videos', dest: 'assets' },
+      ],
+    }),
   ],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     rollupOptions: {
       input: 'index.html',
     },
